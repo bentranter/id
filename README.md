8f1d2a78c297feca245e9db17204ded462f7b2f2
Ben Tranter <bwtrante@lakeheadu.ca>
2015-10-27 00:18:33 -0400

    Ugh

--- README.md
+++ README.md
@@ -1,24 +1,15 @@
-Package psa should satisfy a few requirements:
-
-  1. Be passwordless
-  2. Be sessionless
-  3. Be incredibly easy to create new providers/stratgies
-     for.
-  4. Work for any middleware setup/config.
-  5. Work with either cookies localstorage.
-  6. Be easy to set up continuous integration tests for.
-  7. Have good support for every Oauth feature.
-  8. Which means actual support for refresh tokens.
-
-Let's figure out our dreamcode.
+You should be able to do this:
 
 ```go
-http.HandleFunc("/auth/github/authorize", GitHub.Authorize)
-http.HandleFunc("/auth/github/callback", GitHub.Callback)
-```
+package main
 
-Probably gonna need context package to pass around a few important things.
+import "github.com/bentranter/thrill/providers/twitch"
 
-IDEA: For the three-legged Oauth stuff, pass a context over a channel that contains the config. The config can be updated with each value as it becomes satisfied?
+func main() {
+    provider := twitch.New(clientID, clientSecret, callbackURL)
 
-You can take the same approach for CSRF tokens as well? Idk if this works or can really be called sessionless... well the idea I supposed is that if you shut your server down, you can restart without having to worry about users' sessions being lost, or you can add another process??? and the sessions will still work. I think my idea is okay.
+    http.HandleFunc("/auth/twitch/authorize", provider.Authorize)
+    http.HandleFunc("/auth/twitch/callback", provider.Callback)
+
+    http.ListenAndServe(":3000", nil)
+}
--- providers/twitch/twitch.go
+++ providers/twitch/twitch.go
@@ -1,7 +1,6 @@
 package twitch
 
 import (
-	"log"
 	"net/http"
 	"os"
 
@@ -9,35 +8,56 @@ import (
 )
 
 const (
-	authURL      string = "https://api.twitch.tv/kraken/oauth2/authorize"
-	tokenURL     string = "https://api.twitch.tv/kraken/oauth2/token"
-	userEndpoint string = "https://api.twitch.tv/kraken/user"
-
 	// ScopeUserRead allows the client to access the user's
 	// email address and id (for example)
 	ScopeUserRead string = "user_read"
 )
 
+// New returns a new provider.
+func New() *Provider {
+	return &Provider{
+		ClientID:     os.Getenv("TWITCH_KEY"),
+		ClientSecret: os.Getenv("TWITCH_SECRET"),
+		CallbackURL:  "http://localhost:3000/auth/twitch/callback",
+		AuthURL:      "https://api.twitch.tv/kraken/oauth2/authorize",
+		TokenURL:     "https://api.twitch.tv/kraken/oauth2/token",
+		IdentityURL:  "https://api.twitch.tv/kraken/user",
+	}
+}
+
+type Provider struct {
+	ClientID     string
+	ClientSecret string
+	CallbackURL  string
+	AuthURL      string
+	TokenURL     string
+	IdentityURL  string
+}
+
+func (p *Provider) Authorize(w http.ResponseWriter, r *http.Request) {
+	psa.BuildAuthURL(p)
+}
+
 // Authorize does the whole process. It authenticates with
 // the auth provider, and sets a cookie for our middleware
 // to deal with.
-func Authorize(w http.ResponseWriter, r *http.Request) {
-	// Redirect them to the sign in URL
-	config := psa.NewConfig(os.Getenv("TWITCH_KEY"), os.Getenv("TWITCH_SECRET"), "http://localhost:3000/auth/twitch/callback", authURL, tokenURL)
-	url := config.BuildAuthURL()
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-}
+// func Authorize(w http.ResponseWriter, r *http.Request) {
+// 	// Redirect them to the sign in URL
+// 	config := psa.NewConfig(os.Getenv("TWITCH_KEY"), os.Getenv("TWITCH_SECRET"), "http://localhost:3000/auth/twitch/callback", authURL, tokenURL)
+// 	url := config.BuildAuthURL()
+// 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+// }
 
-// Callback handles the rest.
-func Callback(w http.ResponseWriter, r *http.Request) {
-	code := r.URL.Query().Get("code")
-	config := psa.NewConfig(os.Getenv("TWITCH_KEY"), os.Getenv("TWITCH_SECRET"), "http://localhost:3000/auth/twitch/callback", authURL, tokenURL)
-	url := config.BuildTokenURL(code)
-	req, err := http.NewRequest("POST", tokenURL, url)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatalf("It's broken: %s\n", err)
-	}
-	log.Printf("Response: %+v\n", resp)
-	defer resp.Body.Close()
-}
+// // Callback handles the rest.
+// func Callback(w http.ResponseWriter, r *http.Request) {
+// 	code := r.URL.Query().Get("code")
+// 	config := psa.NewConfig(os.Getenv("TWITCH_KEY"), os.Getenv("TWITCH_SECRET"), "http://localhost:3000/auth/twitch/callback", authURL, tokenURL)
+// 	url := config.BuildTokenURL(code)
+// 	req, err := http.NewRequest("POST", tokenURL, url)
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		log.Fatalf("It's broken: %s\n", err)
+// 	}
+// 	log.Printf("Response: %+v\n", resp)
+// 	defer resp.Body.Close()
+// }
