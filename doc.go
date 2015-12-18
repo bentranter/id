f7482ff73bee2fa60920b56db88e1aa086087964
Ben Tranter <bwtrante@lakeheadu.ca>
2015-12-17 19:06:13 -0500

    Add copyright notice to see if renders on GoDoc.org

--- doc.go
+++ doc.go
@@ -1,3 +1,9 @@
-// Package id is an authentication package that is
-// sessionless, and passwordless.
+// Copyright 2015 Ben Tranter. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+/*
+Package id is an authentication package that is sessionless, and passwordless.
+
+*/
 package id
